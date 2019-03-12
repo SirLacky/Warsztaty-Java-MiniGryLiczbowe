815989b2e11b8f9bfc1687f731a1a9aa4d5335bb
SirLacky <sirlacky@gmail.com>
2019-03-12 22:07:10 +0100

    Wprowadzono pobieranie naglowkow i konwersje na stringa

--- src/pl/sirlacky/lkoziara/workshops/most_popular_words/MostPopularWordSearcherApp.java
+++ src/pl/sirlacky/lkoziara/workshops/most_popular_words/MostPopularWordSearcherApp.java
@@ -1,26 +1,58 @@
-//package pl.sirlacky.lkoziara.workshops.most_popular_words;
-//
-//import java.io.IOException;
-//import java.sql.Connection;
-//import org.jsoup.Jsoup;
-//import org.jsoup.nodes.Document;
-//import org.jsoup.nodes.Element;
-//import org.jsoup.select.Elements;
-//
-//public class MostPopularWordSearcherApp {
-//    public static void main(String[] args) {
-//
-//        Connection connect = Jsoup.connect("https://www.tvn24.pl");
-//    try{
-//        Document document = ((org.jsoup.Connection) connect).get();
-//        Elements links = document.select("span.title");
-//        for (Element elem :links){
-//            System.out.println(elem.text());
-//        }
-//
-//    }catch(IOException e){
-//        e.printStackTrace();
-//    }
-//
-//    }
-//}
+package pl.sirlacky.lkoziara.workshops.most_popular_words;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+public class MostPopularWordSearcherApp {
+    public static void main(String[] args) {
+
+        Connection connect = Jsoup.connect("https://www.onet.pl/");
+
+        String[] excluded = new String[]{"oraz", "ponieważ", "albo",};
+        String words = "";
+
+        //SKANUJE PODANA STRONE I DODAJE ZNALEZIONE SLOWA DO PLIKU popular_words.txt
+
+        try {
+            Document document = ((org.jsoup.Connection) connect).get();
+
+            PrintWriter printWriter = new PrintWriter("/home/sirlacky/Pulpit/Moje Projekty/Warsztaty-Java-MiniGryLiczbowe/src/pl/sirlacky/lkoziara/workshops/most_popular_words/popular_words.txt");
+            Elements links = document.select("span.title");
+            for (Element elem : links) {
+                printWriter.println(elem.text());
+            }
+
+         //TWORZY STRINGA ZAWIERAJACEGO WSZYSTKIE SLOWA
+
+            File file = new File("/home/sirlacky/Pulpit/Moje Projekty/Warsztaty-Java-MiniGryLiczbowe/src/pl/sirlacky/lkoziara/workshops/most_popular_words/popular_words.txt");
+            Scanner scan = new Scanner(file);
+            while (scan.hasNextLine()) {
+                words += scan.nextLine();
+            }
+         //PRZEPISANIE STRINGA DO TABLICY UWZGLEDNIAJAC SLOWA FILTROWANE
+
+            
+
+
+        } catch (FileNotFoundException fnfe) {
+            System.err.println("Nie znaleziono plku");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
+
+
